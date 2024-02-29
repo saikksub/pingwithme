32fe5cdd471f2734be6d32c6d1311142b5529d42
umesh1702 <umeshdevineni1>
2024-02-29 11:35:43 +0530

    all the changes made ever

---
+++ layouts/auth2.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-auto flex items-center justify-start relative p-12 md:h-[100lvh] md:justify-center md:items-center md:relative">
+      <UCard class="w-full max-w-sm">
+          <template>
+              <h1 class="text-sm pb-2 " >Pingwithme</h1>
+          </template>
+          <slot />
+      </UCard>
+  </div>
+</template>
